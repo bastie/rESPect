a85e4c96f83b10421591dc77ca64c58b4d8ca3ac
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2023-09-10 17:32:38 +0200

    simple property reading

--- src/rESPect32.cpp
+++ src/rESPect32.cpp
@@ -1,1 +1,44 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+
 #include "rESPect32.h"
+#include <string.h>
+
+
+/*!
+ @brief rESPect Fassade für integrierte APIs.
+ 
+ rESPect ist eine Fassade, welche unterliegende APIs kapselt.
+ 
+ @author Sebastian Ritter
+ @version 0.0.1 2023-09-10
+
+ */
+public class rESPect {
+  
+  /**
+   @brief set a property for rESPect facade. This is useful.
+   
+   @param name the name of property
+   @param value the value of property
+   
+   @return zero if set was succesful, otherwise none zero value
+   
+   @code
+   int rRc = rSet ("Erni", "Bert");
+   @endcode
+   */
+  int rSet (const char* name, const char* value) {
+    
+    return 1;
+  }
+  
+  const char* rGet (const char* name) {
+    
+    if (strcmp(name, "rVERSION")) {
+      return RESPECT_VERSION;
+    }
+  }
+}
--- src/rESPect32.h
+++ src/rESPect32.h
@@ -1,6 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: MIT
+ */
+
 #ifndef _rESPect32_H_
 #define _rESPect32_H_
 
-#define RESPECT_VERSION "0.0.1"
+#define RESPECT_VERSION "0.0.1" ///< The version of rESPect API
+#include <string.h>
+
+
+class rESPect
+{
+public:
+  const char* rGet(const char* str2);
+};
 
 #endif // _rESPect32_H_
